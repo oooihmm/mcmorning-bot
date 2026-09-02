d86fec694b91ac683f48229632a2eba7d6f315cc
jinseonchoi <sttaurt@gmail.com>
2026-09-02 17:50:47 +0900

    feat: 스레드 작성 테스트

--- bot.py
+++ bot.py
@@ -8,8 +8,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TOKEN = os.getenv("DISCORD_TOKEN")
+WAKE_CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
 
 intents = discord.Intents.default()
+intents.message_content = True
 
 bot = commands.Bot(
     command_prefix="!",
@@ -29,5 +31,30 @@ async def attendance(interaction: discord.Interaction):
         f"☕ {interaction.user.mention}님 출석 완료!"
     )
 
+@bot.tree.command(
+    name="기상게시글",
+    description="기상 인증 게시글을 생성합니다."
+)
+async def create_wake_post(interaction: discord.Interaction):
+
+    channel = bot.get_channel(WAKE_CHANNEL_ID)
+
+    if channel is None:
+        await interaction.response.send_message(
+            "❌ 기상인증 채널을 찾을 수 없습니다.",
+            ephemeral=True
+        )
+        return
+
+    thread = await channel.create_thread(
+        name="☀️ 오늘의 기상 인증",
+        content="8:00까지 이 게시글에 답글을 달아주세요!"
+    )
+
+    await interaction.response.send_message(
+        f"☀️ 기상 인증 게시글을 생성했습니다!\n"
+        f"{thread.thread.jump_url}",
+        ephemeral=True
+    )
 
 bot.run(TOKEN)
