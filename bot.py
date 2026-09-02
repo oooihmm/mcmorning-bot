c23583c52f3845318e2d92f196ad666fd46862e8
jinseonchoi <sttaurt@gmail.com>
2026-09-02 21:00:36 +0900

    feat: 기상 인증 게시판 답글 작성자, 작성 시간

--- bot.py
+++ bot.py
@@ -1,22 +1,21 @@
 import os
+from zoneinfo import ZoneInfo
 
 import discord
-from discord import app_commands
 from discord.ext import commands
 from dotenv import load_dotenv
+
 
 load_dotenv()
 
 TOKEN = os.getenv("DISCORD_TOKEN")
 WAKE_CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
+wake_thread_id = None
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(
-    command_prefix="!",
-    intents=intents
-)
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 
 @bot.event
@@ -31,30 +30,46 @@ async def attendance(interaction: discord.Interaction):
         f"☕ {interaction.user.mention}님 출석 완료!"
     )
 
-@bot.tree.command(
-    name="기상게시글",
-    description="기상 인증 게시글을 생성합니다."
-)
+
+@bot.tree.command(name="기상게시글", description="기상 인증 게시글을 생성합니다.")
 async def create_wake_post(interaction: discord.Interaction):
 
     channel = bot.get_channel(WAKE_CHANNEL_ID)
 
     if channel is None:
         await interaction.response.send_message(
-            "❌ 기상인증 채널을 찾을 수 없습니다.",
-            ephemeral=True
+            "❌ 기상인증 채널을 찾을 수 없습니다.", ephemeral=True
         )
         return
 
     thread = await channel.create_thread(
-        name="☀️ 오늘의 기상 인증",
-        content="8:00까지 이 게시글에 답글을 달아주세요!"
+        name="☀️ 오늘의 기상 인증", content="8:00까지 이 게시글에 답글을 달아주세요!"
     )
 
+    global wake_thread_id
+    wake_thread_id = thread.thread.id
+
     await interaction.response.send_message(
-        f"☀️ 기상 인증 게시글을 생성했습니다!\n"
-        f"{thread.thread.jump_url}",
-        ephemeral=True
+        f"☀️ 기상 인증 게시글을 생성했습니다!\n{thread.thread.jump_url}", ephemeral=True
     )
+
+
+@bot.event
+async def on_message(message: discord.Message):
+
+    if message.author.bot:
+        return
+
+    if message.channel.id != wake_thread_id:
+        return
+
+    kst_time = message.created_at.astimezone(ZoneInfo("Asia/Seoul"))
+
+    print("☀️ 기상 인증 발견!")
+    print(f"작성자: {message.author}")
+    print(f"작성시간: {kst_time}")
+
+    await bot.process_commands(message)
+
 
 bot.run(TOKEN)
